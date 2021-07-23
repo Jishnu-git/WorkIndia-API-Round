82fb1ee356357500a1ca65e9e3f90ff08dfa8307
Jishnu <jishnu30alamelu26@gmail.com>
2021-07-23 13:50:54 +0530

    untested commit

---
+++ Services/CypherService.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const crypto = require("crypto");
+
+const password = crypto.scryptSync("workindiaapiroundsubmissionabcde", "salt", 32);
+const iv = Buffer.from("workindiapiround", "utf-8");
+const cipher = crypto.createCipheriv(
+    "aes-256-cbc",
+    password,
+    iv
+);
+const decipher = crypto.createDecipheriv(
+    "aes-256-cbc",
+    password,
+    iv
+);
+
+
+function encryptText(text) {
+    var encryptedText = cipher.update(text);
+    encryptedText = Buffer.concat([encryptedText, cipher.final()]);
+    return encryptText.toString("hex"); 
+}
+
+function decryptText(hex) {
+    var decryptedText = decipher.update(hex);
+    decryptedText = Buffer.concat([decryptedText, decipher.final()]);
+    return decryptedText.toString();
+}
+
+module.exports = {
+    encryptText: encryptText,
+    decryptText: decryptText
+};
---
+++ main.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("Static/HTML"));
+app.use(express.static("Static/JS"));
+app.use(session({
+    secret: "WorkIndia",
+    saveUninitialized: false,
+    resave: false,
+    unset: "destroy",
+    cookie: {maxAge: 3600 * 24 * 1000}
+}));
+
+
+const mysql = require("mysql");
+const db = mysql.createConnection({
+    host: "localhost",
+    password: "",
+    user: "root",
+    database: "workindia"
+});
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+
+    console.log("successfully connected");
+});
+
+const Cypher = require("./Services/CypherService");
+
+app.post("app/login/", (req, res) => {
+    const user = req.body;
+    db.query("SELECT COUNT(*) AS valid FROM users WHERE username = ? AND password = ?", [user.username, user.password], (err, result) => {
+        if (err || result[0].valid == 0) {
+            res.json({
+                status: "failure",
+                reason: err | "Invalid username"
+            });
+        }
+        req.session.user = user.username;
+            res.json({
+                status: "success"
+            });
+    });
+});
+
+app.post("app/logout/", (req, res) => {
+    req.session.user = null;
+    res.json({
+        status: "success"
+    });
+})
+
+app.get("app/sites/list", (req, res) => {
+    const user = req.session.user | "test";
+    if (!user) {
+        res.json({
+            status: "failure",
+            reason: "session timed out"
+        });
+    } else {
+        db.query(`SELECT website, website_username as user_id, website_password as password
+                  FROM saved_passwords WHERE username = ?`, [user], (err, result) => {
+                     if (err) {
+                         res.json({
+                            status: "failure",
+                            reason: err
+                         });
+                     } else {
+                         res.json(result);
+                     }   
+                  });
+    }
+});
+
+app.post("app/sites", (req, res) => {
+    const user = req.session.user | "test";
+    const password = req.body;
+    if (!user) {
+        res.json({
+            status: "failure",
+            reason: "session timed out"
+        });
+    } else {
+        db.query("INSERT INTO saved_passwords VALUES(?, ?, ?, ?)", [password.website, password.username, Cypher.encryptText(password.password), user],
+        (err, result) => {
+            if (err) {
+                res.json({
+                    status: "failure",
+                    reason: err
+                });
+            } else {
+                res.json({
+                    status: "success"
+                });
+            }
+        });
+    }
+})
+
